ddef5ab5bcda9236c46640ac73b339df7633168b
Endrit <endrit.makolli7@gmail.com>
2022-07-25 12:38:49 +0200

    Front-end walking skeleton added

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,11 +28,13 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -52,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));  //x = policy, allow any type of method from this origin
 
             app.UseAuthorization();
 
