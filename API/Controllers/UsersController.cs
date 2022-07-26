179ce8bc55ee095b0227f77b91c99bb72056d9a0
Endrit <endrit.makolli7@gmail.com>
2022-07-26 13:31:10 +0200

    Basic Authentication using Tokens, Hashing&Salting

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,15 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -23,10 +22,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
             return await _context.Users.ToListAsync();
         }
-
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id){
            return await _context.Users.FindAsync(id);
